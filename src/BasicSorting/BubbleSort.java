77044ed33fe3068e11cdee65d5edc2b7067924b3
Swapnil phad <swapnilphad07@gmail.com>
2026-01-10 00:01:45 +0530

    initial commit

--- src/BasicSorting/BubbleSort.java
+++ src/BasicSorting/BubbleSort.java
@@ -1,4 +1,30 @@
 package BasicSorting;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        bubbleSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) { // n-1 passes
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) break;
+        }
+    }
 }
+
