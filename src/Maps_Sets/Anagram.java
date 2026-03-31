cca8511ba4778b1187d56a911e47c6ff4c4f0674
Swapnil phad <swapnilphad07@gmail.com>
2026-03-31 23:30:15 +0530

    initial commit

---
+++ src/Maps_Sets/Anagram.java
@@ -0,0 +1,27 @@
+package Maps_Sets;
+
+import java.util.HashMap;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+        System.out.println(areAnagrams(s1, s2));
+    }
+    public static boolean areAnagrams(String s1, String s2) {
+        if(s1.length()!=s2.length()) return false;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            char ch = s1.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<s2.length();i++){
+            char ch = s2.charAt(i);
+            if(!map.containsKey(ch)) return false;
+            int freq = map.get(ch);
+            if(freq==0) return false;
+            map.put(ch,freq-1);
+        }
+        return true;
+    }
+}
