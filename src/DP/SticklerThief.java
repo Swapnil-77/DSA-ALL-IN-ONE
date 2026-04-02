ca777d4682ee90775c2b0815eb6bb6efdffd48f3
Swapnil phad <swapnilphad07@gmail.com>
2026-04-02 23:45:06 +0530

    initial commit

---
+++ src/DP/SticklerThief.java
@@ -0,0 +1,48 @@
+package DP;
+
+public class SticklerThief {
+    public static void main(String[] args) {
+        int arr[] = {6,7,1,30,8,2,4};
+        SticklerThief obj = new SticklerThief();
+        System.out.println(obj.findMaxSum(arr));
+    }
+    public int findMaxSum(int arr[]) {
+        int n = arr.length;
+        int[] dp = new int[3];
+        dp[0] = arr[0];
+        if(n>1) dp[1] = Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[2] = Math.max(arr[i]+dp[0],dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[1];
+    }
+
+//    public int findMaxSum(int arr[]) {
+//        int n = arr.length;
+//        int[] dp = new int[n];
+//        dp[0] = arr[0];
+//        if(n>1) dp[1] = Math.max(arr[0],arr[1]);
+//        for(int i=2;i<n;i++){
+//            dp[i] = Math.max(arr[i]+dp[i-2],dp[i-1]);
+//        }
+//        return dp[n-1];
+//    }
+
+//    static int[] dp;
+//    public int findMaxSum(int arr[]) {
+//        int n = arr.length;
+//        dp = new int[n]; // 0 to n-1
+//        Arrays.fill(dp,-1); // mark
+//        return loot(0,arr);
+//    }
+//
+//    private int loot(int i, int[] arr) { // i -> 0 to n-1
+//        if(i>=arr.length) return 0;
+//        if(dp[i]!=-1) return dp[i];
+//        int pick = arr[i] + loot(i+2,arr);
+//        int skip = loot(i+1,arr);
+//        return dp[i] = Math.max(pick,skip);
+//    }
+}
