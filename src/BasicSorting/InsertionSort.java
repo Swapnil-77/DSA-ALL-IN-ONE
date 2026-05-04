33bc3b157e334bb38d923fd76dd79aa594c141cc
Swapnil phad <swapnilphad07@gmail.com>
2026-05-04 23:02:46 +0530

    initial commit

--- src/BasicSorting/InsertionSort.java
+++ src/BasicSorting/InsertionSort.java
@@ -3,7 +3,7 @@ package BasicSorting;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {64, 25, 12, 22, 11};
-       // Insertion sort algorithm
+       // Insertion sort algorithms
         for (int i = 1; i < arr.length; i++) {
             int j = i;
             // Move elements of arr[0..i-1], that are greater than key,
