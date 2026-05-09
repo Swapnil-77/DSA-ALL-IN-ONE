aa48c4448bbc20438a12c2e34c9f033f83e27e62
Swapnil phad <swapnilphad07@gmail.com>
2026-05-09 23:56:36 +0530

    initial commit

--- src/DP/CountDearrangement.java
+++ src/DP/CountDearrangement.java
@@ -4,7 +4,7 @@ public class CountDearrangement {
 
     public static void main(String[] args) {
         CountDearrangement obj = new CountDearrangement();
-        int n = 5;
+        int n = 7;
         System.out.println(obj.countDer(n));
     }
     // 🔹 Memoization (Top-Down DP, O(n) space) 🔹
